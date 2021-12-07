d517790c11a07f42eef96bfc493fe55386b2efd3
HuiGong-dev <hellohuigong@gmail.com>
2021-12-07 20:18:26 +0100

    visualize motion data

--- app.js
+++ app.js
@@ -16,7 +16,29 @@ function getAccel() {
                 frontToBack_degrees = event.beta;
                 leftToRight_degree = event.gamma;
                 console.log("rotation: " + rotation_degrees 
-                + " front to back degrees: "+ frontToBack_degrees + " left to right degree " + leftToRight_degree);
+                + " front to back degrees: " + frontToBack_degrees 
+                + " left to right degree: " + leftToRight_degree);
+
+                vx += leftToRight_degree * refreshRate;
+                vy += frontToBack_degrees * refreshRate;
+
+                px += vx * 0.3;
+                if (px > 98 || px < 0) {
+                    px = Math.max(0, Math.min(98, px));
+                    vx = 0;
+                }
+
+                py += vy * 0.3;
+                if (py > 98 || py <0) {
+                    py = Math.max(0, Math.min(98, py));
+                    vy = 0;
+                }
+
+                dot = document.getElementsByClassName("indicatorDot")[0];
+                dot.setAttribute('style', "left:" + (px) + "%;" + "top:" + (py) + "%")
+
+
+
                 
             })
         }
