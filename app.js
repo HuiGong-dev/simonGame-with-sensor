0b2ebc0bfad3b95d805bb83c0fd0d2c53a8d6ad8
HuiGong-dev <hellohuigong@gmail.com>
2021-12-07 22:03:49 +0100

    fix

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ function getAccel() {
                 vy += frontToBack_degrees * refreshRate * 0.5;
 
                 px += vx * 0.3;
-                if (px > 95 || px < 0) {
-                    px = Math.max(0, Math.min(95, px));
+                if (px > 93 || px < 0) {
+                    px = Math.max(0, Math.min(93, px));
                     vx = 0;
                 }
 
