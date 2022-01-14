2d65837389b39e69416ee73af075d2f410c43f82
HuiGong-dev <hellohuigong@gmail.com>
2022-01-14 10:42:18 +0100

    Revert "fix scope"
    This reverts commit 15b704b95883ed6c615985b583b3f6dc5a8a953a.

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ function nextSequence() {
     level++;
     $("#level-title").text("Level " + level);
 
-    let randomNumber = Math.floor(Math.random() * 4);
-    let randomLocation = ballLocations[randomNumber];
+    const randomNumber = Math.floor(Math.random() * 4);
+    const randomLocation = ballLocations[randomNumber];
     gamePattern.push(randomLocation);
     $("#" + randomLocation).fadeIn(200).fadeOut(200).fadeIn(200);
     
@@ -76,7 +76,7 @@ function checkAnswer(currentLevel) {
         setTimeout(function () {
             $("body").removeClass("game-over");
         }, 200);
-        let achievedLevel = level;
+        const achievedLevel = level;
 
         startOver();
         // window.removeEventListener('deviceorientation', handleOrientation);
