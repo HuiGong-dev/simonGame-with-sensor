eb89f87acd801c21b7b5541df5d924af0a17ff27
HuiGong-dev <hellohuigong@gmail.com>
2022-01-24 13:58:25 +0100

    fix local fonts

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./fonts/press-start-2p-v9-latin-regular.eot", "./src/styles.css","./src/jquery.min.js", "./src/app.js", "./icons/manifest-icon-192.maskable.png"]);
+            return cache.addAll(["./",
+             "./fonts/press-start-2p-v9-latin-regular.eot",
+             "./fonts/press-start-2p-v9-latin-regular.svg",
+             "./fonts/press-start-2p-v9-latin-regular.ttf",
+             "./fonts/press-start-2p-v9-latin-regular.woff",
+             "./fonts/press-start-2p-v9-latin-regular.woff2",
+             "./src/styles.css",
+             "./src/jquery.min.js",
+             "./src/app.js",
+             "./icons/manifest-icon-192.maskable.png"]);
         })
     );
 });
