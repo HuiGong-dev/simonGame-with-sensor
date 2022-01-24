d7d6da27916d7b58de15d6b881526c4b0608da08
HuiGong-dev <hellohuigong@gmail.com>
2022-01-24 13:40:03 +0100

    add local fonts

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./src/styles.css","./src/jquery.min.js", "./src/app.js", "./icons/manifest-icon-192.maskable.png"]);
+            return cache.addAll(["./", "./fonts/", "./src/styles.css","./src/jquery.min.js", "./src/app.js", "./icons/manifest-icon-192.maskable.png"]);
         })
     );
 });
